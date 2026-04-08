ac1c63d38c8bde74ce2cf3c693ca353a56dbf601
anna karolyne <129319846+AnnaKArolynee@users.noreply.github.com>
2026-04-08 11:48:29 -0300

    Create testeseis class to display string characters
    This program demonstrates how to access specific characters in a string.

---
+++ exerciciocinco.java
@@ -0,0 +1,14 @@
+public class testeseis {
+    public static void main(String[] args) {
+        String x = "computação";
+
+        char lendo = x.charAt(0);
+        char lendodois = x.charAt(2);
+        char lendotres = x.charAt(9);
+
+        System.out.println("A palavra computação mostrando o primeiro, terceiro e último caractere");
+        System.out.println(lendo);
+        System.out.println(lendodois);
+        System.out.println(lendotres);
+    }
+}
